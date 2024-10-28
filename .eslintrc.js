31bf913c4c273a5cb0800e69224c36f753b7ed16
Thinh Nguyen <thinhnn@cystack.net>
2024-10-28 15:38:58 +0700

    chore: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rule: {
+    'react-native/no-inline-styles': true,
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
   singleQuote: true,
-  trailingComma: 'all',
-};
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5"
+}
